fe6bc57ae954f0b5f487a1d2f7ae3d9258a5cc9a
arokenny <arokennyrudge@gmail.com>
2025-08-04 15:19:00 +0800

    master

---
+++ src/banking/BankingClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking;
+import java.util.Scanner;
+
+public class BankingClass {
+    int accountNo = 10001;
+    String name;
+    float initialBalance = 0;
+    int pin = 1234;
+    
+    public boolean verifyAccount(int acc, int pn){
+        
+            if(acc == accountNo && pn == pin){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    
+    public int setAccount(){
+        return accountNo;
+    }
+    
+    public void viewBalance(){
+    
+    
+    }
+}
